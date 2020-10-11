820fc07ca6c9afc9fc0b47ccb05ea54a4bb37aaf
xdiegoneves <diego_neves@usp.br>
2020-10-11 18:41:26 -0300

    ajuste de arquivos

--- test/models/raw_registros_ocorrencias_ssp_sp_gov_br_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RawRegistrosOcorrenciasSspSpGovBrTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/raw_registros_ocorrencias_ssp_sp_gov_brs_teste_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RawRegistrosOcorrenciasSspSpGovBrsTesteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
