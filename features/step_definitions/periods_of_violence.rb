905f19499c52ade694da07d589fd9095a9b23ee6
felipe.dias <felipe.dias@creditas.com.br>
2020-10-24 17:51:09 -0300

    behaviour refactoring

--- features/step_definitions/periods_of_violence.rb
+++ features/step_definitions/periods_of_violence.rb
@@ -1,11 +1,10 @@
 Dado('que estou na página sobre período de violências contra mulheres') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/periods_of_violence'
 end
 
 Quando('faço nada') do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
-Então('uma tabela com os períodos de violência contra mulheres é exibida') do
-  pending # Write code here that turns the phrase above into concrete actions
+Então(/^a tabela "([^"]*)" contra mulheres é exibida$/) do |arg|
+  pending
 end
