9cebd847ba794c150152c552c52eee82942e77af
felipe.dias <felipe.dias@creditas.com.br>
2020-10-24 17:13:56 -0300

    behaviour tests set up

---
+++ features/step_definitions/periods_of_violence.rb
@@ -0,0 +1,11 @@
+Dado('que estou na página sobre período de violências contra mulheres') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('faço nada') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('uma tabela com os períodos de violência contra mulheres é exibida') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
