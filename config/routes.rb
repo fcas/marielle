7bcae06839e5066aa77ea9b1f01e0a042f7b333d
felipe.dias <felipe.dias@creditas.com.br>
2020-10-25 00:48:40 -0300

    - adding periods_of_violence to routes - adding periods_of_violence views - adding periods_of_violence controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :raw_registros_ocorrencias_ssp_sp_gov_brs
   root 'welcome#index'
+  namespace :reports do
+    get "periods_of_violence", to: "reports/periods_of_violence"
+  end
 end
