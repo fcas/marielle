8d440f66247218b051e1264acc42bbad2e4aa3dd
Larissa Amaral <larissa95.amaral@gmail.com>
2020-11-11 08:31:53 -0300

    remove requiring simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'coveralls'
-require 'simplecov'
+#require 'simplecov'
 
-Coveralls.wear_merged!
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: 1)
+  parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
