fc57a70fae04b887eebd1dbff2002c0884dba7d6
felipe.dias <felipe.dias@creditas.com.br>
2020-12-06 11:18:12 -0300

    - refactoring behavior tests

--- features/cadastrar_grupo_reflexiivo.feature
+++ features/cadastrar_grupo_reflexiivo.feature
@@ -7,13 +7,13 @@ de forma a disponibilizar esses dados para a população.
 
 Cenário: Cadastro de grupo reflexivo
 Dado que estou na página de cadastro de grupo reflexivo
-Quando preencho o campo "telefone" com "1234567" do formulário do grupo reflexivo
+Quando preencho o campo "telefone" com "11111111111" do formulário do grupo reflexivo
 E preencho o campo "logradouro" com "Avenida Paulista" do formulário do grupo reflexivo
 E preencho o campo "numero" com "113" do formulário do grupo reflexivo
 E preencho o campo "bairro" com "Cerqueira César" do formulário do grupo reflexivo
 E preencho o campo "cidade" com "São Paulo" do formulário do grupo reflexivo
 E seleciono a opção "SP" no campo "uf" do formulário do grupo reflexivo
 E preencho o campo "nomegruporeflexivo" com "MEMOH" do formulário do grupo reflexivo
-E clico em salvar
+E clico em salvar grupo reflexivo
 Então o grupo reflexivo deve ter sido salvo no banco de dados
 E deverei ver a página de detalhamento do grupo reflexivo
--- features/step_definitions/cadastrar_grupo_reflexivo.rb
+++ features/step_definitions/cadastrar_grupo_reflexivo.rb
@@ -3,21 +3,21 @@ Dado('que estou na página de cadastro de grupo reflexivo') do
 end
 
 Quando('preencho o campo {string} com {string} do formulário do grupo reflexivo') do |campo, valor|
-  find('#grupo_reflexivo_' + campo).send_keys(valor)
+  find('#registros_grupos_reflexivo_' + campo).send_keys(valor)
 end
 
 Quando('seleciono a opção {string} no campo {string} do formulário do grupo reflexivo') do |campo, valor|
-    find('#grupo_reflexivo_' + valor)
+    find('#registros_grupos_reflexivo_' + valor)
         .find('option[value="' + campo + '"]').select_option
 end
 
-Quando('clico em salvar') do
+Quando('clico em salvar grupo reflexivo') do
     click_on 'Registrar'
 end
 
 Então('o grupo reflexivo deve ter sido salvo no banco de dados') do
-    grupo_reflexivo = RawGrupoReflexivo.order("id").last
-    expect(grupo_reflexivo.telefone).to eq('1234567')
+    grupo_reflexivo = RegistrosGruposReflexivo.order("id").last
+    expect(grupo_reflexivo.telefone).to eq('(11) 11111-1111')
     expect(grupo_reflexivo.logradouro).to eq('Avenida Paulista')
     expect(grupo_reflexivo.numero).to eq(113)
     expect(grupo_reflexivo.bairro).to eq('Cerqueira César')
@@ -26,10 +26,8 @@ Então('o grupo reflexivo deve ter sido salvo no banco de dados') do
     expect(grupo_reflexivo.nomegruporeflexivo).to eq('MEMOH')
 end
 
-Então('o grupo reflexivo deve ter sido salvo no banco de dados') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
 Então('deverei ver a página de detalhamento do grupo reflexivo') do
-    pending # Write code here that turns the phrase above into concrete actions
+    grupo_reflexivo = RegistrosGruposReflexivo.order("id").last
+    id = grupo_reflexivo.id
+    visit('/registros_grupos_reflexivos/' + id)
 end
